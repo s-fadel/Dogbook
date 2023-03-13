155d4df8348015dac69c944d9e3e344dc7d3858e
stephanie <stephanie.fadel@hotmail.com>
2023-03-13 19:51:57 +0100

    Add delete handler

--- src/App.js
+++ src/App.js
@@ -4,48 +4,40 @@ import Start from "./Start";
 import Create from "./Create";
 
 const App = () => {
+  const localStorageKey = "schoolDogs";
 
-  const localStorageKey = "schoolDogs"
-
-  const savedDogs = JSON.parse(localStorage.getItem(localStorageKey))
-  console.log(savedDogs)
-  const [view, setView] = useState("HOME")
+  const savedDogs = JSON.parse(localStorage.getItem(localStorageKey));
+  console.log(savedDogs);
+  const [view, setView] = useState("HOME");
   const [dogs, setDogs] = useState(savedDogs || []);
   const [dog, setDog] = useState({});
   const [edit, setEdit] = useState({});
 
- 
-  useEffect (() => {
-    if(dog.id) {
-      const newDogs = dogs.map(d => 
-        {
-          if(d.id === dog.id){
-            return dog
-          }
-        } )
-      setDogs(newDogs)
+  useEffect(() => {
+    if (dog.id) {
+      const newDogs = dogs.map((d) => {
+        if (d.id === dog.id) {
+          return dog;
+        }
+      });
+      setDogs(newDogs);
     }
+  }, [dog]);
 
-  }, [dog])
+  useEffect(() => {
+    localStorage.setItem(localStorageKey, JSON.stringify(dogs));
+  }, [dogs]);
 
-
-  useEffect (() => {
-    localStorage.setItem(localStorageKey, JSON.stringify(dogs))
-  }, [dogs])
-
-/*   */
-
-switch (view) {
-  case "HOME":
-    return <Start setView={setView} dogs={dogs} setDog={setDog} />
-  /* case "DOG":
+  switch (view) {
+    case "HOME":
+      return <Start setView={setView} dogs={dogs} setDogs={setDogs} />;
+    /* case "DOG":
     return <Dog setView={setView} dog={dog} />
   case "EDIT":
     return <EditDog setView={setView} setEdit={setEdit} dog={dog} setDog={setDog} /> */
-  default:
-    return <Create setView={setView} setDogs={setDogs} dogs={dogs}/>
-}
-  
-}
+    default:
+      return <Create setView={setView} setDogs={setDogs} dogs={dogs} />;
+  }
+};
 
 export default App;
--- src/Start.js
+++ src/Start.js
@@ -1,31 +1,27 @@
 const Start = ({ setView, dogs, setDogs }) => {
   const handleDelete = (id) => {
-    const newDogs = dogs.filter((Dog) => Dog.id !== id);
+    const newDogs = dogs.filter((dog) => dog.id !== id);
     setDogs(newDogs);
-  };
+  }
 
-  /*   const DayCare = () => {
-    const [dogsInDaycare, setDogsInDaycare] = useState([]);
-
-  } */
   return (
     <div className="dog-school">
       <h1>DOG SCHOOL🐶🏫</h1>
       <p>Welcome to the dog mafia! Search for a dog to check in for the day.</p>
       <div className="dog-list">
-      
         <h4>DOGS OF THE DAY: {dogs.length}</h4>
-       
-        {dogs.length ? dogs.map((dogHome) => (
-          <>
-          <div className="dog-container">
-           <span className="dog-nickname">{dogHome.nickname} </span>{/* Designa här */}
-            <button className="delete-button" onClick={() => handleDelete(dogHome.id)}>X</button>
-            </div>
-          </>
-        )):null}
+        {dogs.map((dog) => (
+          <div className="dog-container" key={dog.id}>
+            <span className="dog-nickname">{dog.nickname}</span>
+            <button
+              className="delete-button"
+              onClick={() => handleDelete(dog.id)}
+            >
+              ❌
+            </button>
+          </div>
+        ))}
       </div>
-      
       <div className="create-new-member">
         <p>New member to the dog mafia?</p>
         <button onClick={() => setView("CREATE")} id="add-dog">
